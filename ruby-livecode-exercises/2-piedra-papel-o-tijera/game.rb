beee48cae155f71094c1154422e050a86d4d6319
stefano amodei <samodei@udesa.edu.ar>
2023-04-12 22:56:52 -0300

    correcting identation

--- ruby-livecode-exercises/2-piedra-papel-o-tijera/game.rb
+++ ruby-livecode-exercises/2-piedra-papel-o-tijera/game.rb
@@ -5,7 +5,7 @@
 # Compare both hands (Computer vs User)
 # Puts who WINS | LOSS | DRAW
 
-hands =  ["rock", "paper", "scissors"]
+hands = ["rock", "paper", "scissors"]
 
 computer_hand = hands.sample
 user_hand = nil
@@ -13,31 +13,31 @@ user_hand = nil
 # puts computer_hand
 
 loop do
-    puts "Please choose your hand: #{hands.join(", ")}"
-    print ">"
-    user_hand = gets.chomp
+  puts "Please choose your hand: #{hands.join(", ")}"
+  print ">"
+  user_hand = gets.chomp
 
-    if hands.include?(user_hand)
-        break
-    else 
-        puts "Wrong choice... try again"
-    end
+  if hands.include?(user_hand)
+    break
+  else
+    puts "Wrong choice... try again"
+  end
 end
 
 
 if computer_hand == "rock"
-    user_win = user_hand == "paper"
+  user_win = user_hand == "paper"
 elsif computer_hand == "paper"
-    user_win = user_hand == "scissors"
+  user_win = user_hand == "scissors"
 elsif computer_hand == "scissors"
-    user_win = user_hand == "rock"
+  user_win = user_hand == "rock"
 end
 
 
 if user_win
-    puts "The computer chose #{computer_hand}, you won"
+  puts "The computer chose #{computer_hand}, you won"
 elsif computer_hand == user_hand
-    puts "The computer chose #{computer_hand}, it's a draw"
-else 
-    puts "The computer chose #{computer_hand}, you lost"
+  puts "The computer chose #{computer_hand}, it's a draw"
+else
+  puts "The computer chose #{computer_hand}, you lost"
 end
