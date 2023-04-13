51dc2a48452678968ecbf44a0c663dfc45ebb7ab
stefano amodei <samodei@udesa.edu.ar>
2023-04-12 22:47:52 -0300

    decrypt exercise done

--- ruby-livecode-exercises/3-acronymize-decripting/encrypt.rb
+++ ruby-livecode-exercises/3-acronymize-decripting/encrypt.rb
@@ -1,15 +1,23 @@
-def encrypt(message)
+def encrypt(message, position = -3)
   alphabet = ("A".."Z").to_a
   message.upcase.split("").map do |letter|
     index_letter = alphabet.index(letter)
-    index_letter.nil? ? letter : alphabet[index_letter - 3]
-    #if index_letter.nil?
+    index_letter.nil? ? letter : alphabet[(index_letter + position) % 26]
+    # if index_letter.nil?
     #  letter
     # else
-      # alphabet[index_letter - 3]
+    # alphabet[index_letter - 3]
     # end
   end.join
 end
 
+def decrypt(message)
+  encrypt(message, 3)
+end
+
+
 p encrypt("Hola a todos")
 # ENCRIPTAMOS EL MENSAJE CORRIENDO 3 LETRAS DEL ABECEDARIO A LA IZQUIERDA
+
+# Ahora lo desencriptamos
+p decrypt("ELIX X QLALP")
